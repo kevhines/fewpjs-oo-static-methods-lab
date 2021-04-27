dae239ebae43aca48af1a08a08276e7f491a96bf
Kevin <kevhines@gmail.com>
2021-04-27 15:48:07 -0400

    commit tree 0298317c4bab93a5fbf422cf5d98d183180c3439

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static smallWord(word) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return smallWords.includes(word.toLowerCase())
+  }
+
+  static titleize(string) {
+    let words = string.split(" ")
+    let newString = this.capitalize(words[0])
+    for (let i = 1; i < words.length; i++) {
+      if (this.smallWord(words[i])) {
+        newString += " " + words[i]
+      } else {
+        newString += " " + this.capitalize(words[i])
+      }
+    }
+    return newString
+  }
 }
